0675c19cfd3410c88c6049065016867e666f7fde
Oliver Stenbom <oliver@stenbom.eu>
2023-03-18 13:13:25 +0100

    get_request_session finds session based on request

--- serpress/src/lib.rs
+++ serpress/src/lib.rs
@@ -51,8 +51,51 @@ pub enum SessionError {
     NoSuchSession(String) // Add known headers to error
 }
 
-pub fn get_request_session<T: SessionStore>(domain: String, headers: HashMap<String, String>, store: &T) -> Result<ServerConfig, SessionError> {
-    Err(SessionError::NoSuchSession(domain))
+pub fn get_request_session<T: SessionStore>(url: String, headers: HashMap<String, String>, store: &T) -> Result<ServerConfig, SessionError> {
+    if let Some(config) = store.get(first_subdomain(&url)) {
+        return Ok(config);
+    }
+
+    if let Some(referer) = headers.get("referer") {
+        if let Some(config) = store.get(first_subdomain(referer)) {
+            return Ok(config);
+        }
+    }
+
+    if let Some(tracestate) = headers.get("tracestate") {
+        if let Some(config) = store.get(extract_tracestate(tracestate)) {
+            return Ok(config);
+        }
+    }
+
+    Err(SessionError::NoSuchSession(url))
+}
+
+fn first_subdomain(url: &String) -> String {
+    let without_schema = url.strip_prefix("http://").or_else(|| url.strip_prefix("https://")).unwrap_or(url);
+    let parts: Vec<&str> = without_schema.split('.').collect();
+    if parts.len() <= 2 {
+        String::from("")
+    } else {
+        String::from(parts[0])
+    }
+}
+
+fn extract_tracestate(tracestate: &String) -> String {
+    tracestate
+        .split(',')
+        .filter_map(|kv| {
+            let mut parts = kv.splitn(2, '=');
+            let key = parts.next()?;
+            let value = parts.next()?;
+            if key.trim() == "serpress-session" {
+                Some(value.trim().to_string())
+            } else {
+                None
+            }
+        })
+        .next()
+        .unwrap_or_else(|| "".to_string())
 }
 
 
@@ -88,6 +131,22 @@ mod tests {
 
         let name = session_store.new(config, NameKind::Animal, None);
 
+        // Normal subdomain
         get_request_session(format!("{}.example.com", name), HashMap::new(), &session_store).unwrap();
+
+        // Referer
+        let mut referer_headers: HashMap<String, String> = HashMap::new();
+        // TODO check header capitalization
+        referer_headers.insert(format!("referer"), format!("http://{}.example.com", name));
+        get_request_session(format!("example.com"), referer_headers, &session_store).unwrap();
+
+        // Trace state
+        let mut trace_headers: HashMap<String, String> = HashMap::new();
+        trace_headers.insert(format!("tracestate"), format!("some-other=xyz,serpress-session={}", name));
+        get_request_session(format!("example.com"), trace_headers, &session_store).unwrap();
+
+        let mut trace_headers_two: HashMap<String, String> = HashMap::new();
+        trace_headers_two.insert(format!("tracestate"), format!("serpress-session={}", name));
+        get_request_session(format!("example.com"), trace_headers_two, &session_store).unwrap();
     }
 }
