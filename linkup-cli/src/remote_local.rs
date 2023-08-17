6f621bd40ee669ddf37290d8d10306d72ef52688
Oliver Stenbom <oliver@stenbom.eu>
2023-08-17 16:24:00 +0200

    Add more useful print statements to most commands (#16)

--- linkup-cli/src/remote_local.rs
+++ linkup-cli/src/remote_local.rs
@@ -1,14 +1,16 @@
+use url::Url;
+
 use crate::{
-    background_booting::boot_background_services,
-    local_config::ServiceTarget,
+    background_booting::{load_config, server_config_from_state},
+    local_config::{LocalState, ServiceTarget},
     start::{get_state, save_state},
-    CliError,
+    CliError, LINKUP_LOCALSERVER_PORT,
 };
 
 pub fn remote(service_name: String) -> Result<(), CliError> {
     let mut state = get_state()?;
 
-    let mut service = state
+    let service = state
         .services
         .iter_mut()
         .find(|s| s.name == service_name)
@@ -18,8 +20,13 @@ pub fn remote(service_name: String) -> Result<(), CliError> {
         )))?;
     service.current = ServiceTarget::Remote;
 
-    save_state(state)?;
-    boot_background_services()?;
+    save_state(state.clone())?;
+    load_server_states(state)?;
+
+    println!(
+        "Linkup is routing {} traffic to the remote server",
+        service_name
+    );
 
     Ok(())
 }
@@ -27,7 +34,7 @@ pub fn remote(service_name: String) -> Result<(), CliError> {
 pub fn local(service_name: String) -> Result<(), CliError> {
     let mut state = get_state()?;
 
-    let mut service = state
+    let service = state
         .services
         .iter_mut()
         .find(|s| s.name == service_name)
@@ -37,7 +44,28 @@ pub fn local(service_name: String) -> Result<(), CliError> {
         )))?;
     service.current = ServiceTarget::Local;
 
-    save_state(state)?;
-    boot_background_services()?;
+    save_state(state.clone())?;
+    load_server_states(state)?;
+
+    println!(
+        "Linkup is routing {} traffic to the local server",
+        service_name
+    );
+
+    Ok(())
+}
+
+fn load_server_states(state: LocalState) -> Result<(), CliError> {
+    let local_url = Url::parse(&format!("http://localhost:{}", LINKUP_LOCALSERVER_PORT))
+        .expect("linkup url invalid");
+
+    let (local_server_conf, remote_server_conf) = server_config_from_state(&state);
+    let _ = load_config(
+        &state.linkup.remote,
+        &state.linkup.session_name.clone(),
+        remote_server_conf,
+    )?;
+    let _ = load_config(&local_url, &state.linkup.session_name, local_server_conf)?;
+
     Ok(())
 }
