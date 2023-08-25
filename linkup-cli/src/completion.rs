a3756680a499dfd5715cefb90dd2d6c6b2ed680a
Fredrik Ygge <fredrik.ygge@mentimeter.com>
2023-08-25 13:23:09 +0200

    move clone

--- linkup-cli/src/completion.rs
+++ linkup-cli/src/completion.rs
@@ -6,15 +6,12 @@ use clap_complete::{generate, Generator, Shell};
 use crate::{Cli, CliError};
 
 pub fn completion(shell: &Option<Shell>) -> Result<(), CliError> {
-    if let Some(shell) = shell.clone() {
+    if let Some(shell) = shell {
         let mut cmd = Cli::command();
         print_completions(shell, &mut cmd);
-        Ok(())
-    } else {
-        Ok(())
     }
+    Ok(())
 }
-
-fn print_completions<G: Generator>(gen: G, cmd: &mut Command) {
-    generate(gen, cmd, cmd.get_name().to_string(), &mut stdout());
+fn print_completions<G: Generator + Clone>(gen: &G, cmd: &mut Command) {
+    generate(gen.clone(), cmd, cmd.get_name().to_string(), &mut stdout());
 }
