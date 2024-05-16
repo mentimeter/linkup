fdef771c270c72ce4c6a2f6d8a600b41084d03fb
Charlotte Andersson <charlotteandersson6@gmail.com>
2024-05-16 17:04:36 +0200

    feat: random tunnel secret test: save credentials

--- linkup-cli/src/tunnel.rs
+++ linkup-cli/src/tunnel.rs
@@ -1,15 +1,17 @@
 use std::{
     env,
-    fs::{self, File},
-    io::{Read, Write},
-    path::{Path, PathBuf},
+    fs::{self},
+    path::Path,
 };
 
-use mockall::automock;
 use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION, CONTENT_TYPE};
 
+use crate::file_system::{FileLike, FileSystem, RealFileSystem};
 use crate::CliError;
 use serde::{Deserialize, Serialize};
+
+use base64::prelude::*;
+use rand::Rng;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct GetTunnelApiResponse {
@@ -106,55 +108,98 @@ fn send_request<T: for<'de> serde::Deserialize<'de>>(
     }
 }
 
-pub fn get_tunnel_id(tunnel_name: &str) -> Result<Option<String>, CliError> {
-    let account_id = env::var("LINKUP_CLOUDFLARE_ACCOUNT_ID")
-        .map_err(|err| CliError::BadConfig(err.to_string()))?;
-    let url = format!(
-        "https://api.cloudflare.com/client/v4/accounts/{}/cfd_tunnel",
-        account_id
-    );
-    let (client, headers) = prepare_client_and_headers()?;
-    let query_url = format!("{}?name=tunnel-{}", url, tunnel_name);
+#[cfg_attr(test, mockall::automock)]
+pub trait TunnelManager {
+    fn get_tunnel_id(&self, tunnel_name: &str) -> Result<Option<String>, CliError>;
+    fn create_tunnel(&self, tunnel_name: &str) -> Result<String, CliError>;
+    fn create_dns_record(&self, tunnel_id: &str, tunnel_name: &str) -> Result<(), CliError>;
+}
 
-    let parsed: GetTunnelApiResponse = send_request(&client, &query_url, headers, None, "GET")?;
-    if parsed.result.is_empty() {
-        Ok(None)
-    } else {
-        Ok(Some(parsed.result[0].id.clone()))
+pub struct RealTunnelManager;
+
+impl TunnelManager for RealTunnelManager {
+    fn get_tunnel_id(&self, tunnel_name: &str) -> Result<Option<String>, CliError> {
+        let account_id = env::var("LINKUP_CLOUDFLARE_ACCOUNT_ID")
+            .map_err(|err| CliError::BadConfig(err.to_string()))?;
+        let url = format!(
+            "https://api.cloudflare.com/client/v4/accounts/{}/cfd_tunnel",
+            account_id
+        );
+        let (client, headers) = prepare_client_and_headers()?;
+        let query_url = format!("{}?name=tunnel-{}", url, tunnel_name);
+
+        let parsed: GetTunnelApiResponse = send_request(&client, &query_url, headers, None, "GET")?;
+        if parsed.result.is_empty() {
+            Ok(None)
+        } else {
+            Ok(Some(parsed.result[0].id.clone()))
+        }
+    }
+
+    fn create_tunnel(&self, tunnel_name: &str) -> Result<String, CliError> {
+        let tunnel_secret = generate_tunnel_secret();
+        let account_id = env::var("LINKUP_CLOUDFLARE_ACCOUNT_ID")
+            .map_err(|err| CliError::BadConfig(err.to_string()))?;
+        let url = format!(
+            "https://api.cloudflare.com/client/v4/accounts/{}/cfd_tunnel",
+            account_id,
+        );
+        let (client, headers) = prepare_client_and_headers()?;
+        let body = serde_json::to_string(&CreateTunnelRequest {
+            name: format!("tunnel-{}", tunnel_name),
+            tunnel_secret: tunnel_secret.clone(),
+        })
+        .map_err(|err| CliError::StatusErr(err.to_string()))?;
+
+        let parsed: CreateTunnelResponse =
+            send_request(&client, &url, headers, Some(body), "POST")?;
+        save_tunnel_credentials(&RealFileSystem, &parsed.result.id, &tunnel_secret)
+            .map_err(|err| CliError::StatusErr(err.to_string()))?;
+        create_config_yml(&RealFileSystem, &parsed.result.id)
+            .map_err(|err| CliError::StatusErr(err.to_string()))?;
+
+        Ok(parsed.result.id)
+    }
+
+    fn create_dns_record(&self, tunnel_id: &str, tunnel_name: &str) -> Result<(), CliError> {
+        //let zone_id = env::var("LINKUP_CLOUDFLARE_ZONE_ID").map_err(|err| CliError::BadConfig(err.to_string()) )?;
+        let zone_id = "ZONE_ID";
+        let url = format!(
+            "https://api.cloudflare.com/client/v4/zones/{}/dns_records",
+            zone_id
+        );
+        let (client, headers) = prepare_client_and_headers()?;
+        let body = serde_json::to_string(&CreateDNSRecordRequest {
+            name: format!("tunnel-{}", tunnel_name),
+            content: format!("{}.cfargotunnel.com", tunnel_id),
+            r#type: "CNAME".to_string(),
+            proxied: true,
+        })
+        .map_err(|err| CliError::StatusErr(err.to_string()))?;
+
+        println!("{}", body);
+
+        send_request(&client, &url, headers, Some(body), "POST")
     }
 }
 
-pub fn create_tunnel(tunnel_name: &str) -> Result<String, CliError> {
-    let tunnel_secret = "AQIDBAUGBwgBAgMEBQYHCAECAwQFBgcIAQIDBAUGBwg=".to_string(); // This is a hardcoded secret, it should be generated
-    let account_id = env::var("LINKUP_CLOUDFLARE_ACCOUNT_ID")
-        .map_err(|err| CliError::BadConfig(err.to_string()))?;
-    let url = format!(
-        "https://api.cloudflare.com/client/v4/accounts/{}/cfd_tunnel",
-        account_id,
-    );
-    let (client, headers) = prepare_client_and_headers()?;
-    let body = serde_json::to_string(&CreateTunnelRequest {
-        name: format!("tunnel-{}", tunnel_name),
-        tunnel_secret: tunnel_secret.clone(),
-    })
-    .map_err(|err| CliError::StatusErr(err.to_string()))?;
-
-    let parsed: CreateTunnelResponse = send_request(&client, &url, headers, Some(body), "POST")?;
-    save_tunnel_credentials(&parsed.result.id, &tunnel_secret)
-        .map_err(|err| CliError::StatusErr(err.to_string()))?;
-    create_config_yml(&RealFileSystem, &parsed.result.id)
-        .map_err(|err| CliError::StatusErr(err.to_string()))?;
-
-    Ok(parsed.result.id)
+fn generate_tunnel_secret() -> String {
+    let mut rng = rand::thread_rng();
+    let random_bytes: [u8; 32] = rng.gen();
+    BASE64_STANDARD.encode(random_bytes)
 }
 
-fn save_tunnel_credentials(tunnel_id: &str, tunnel_secret: &str) -> Result<(), CliError> {
+fn save_tunnel_credentials(
+    fs: &dyn FileSystem,
+    tunnel_id: &str,
+    tunnel_secret: &str,
+) -> Result<(), CliError> {
     let account_id = env::var("LINKUP_CLOUDFLARE_ACCOUNT_ID")
         .map_err(|err| CliError::BadConfig(err.to_string()))?;
     let data = serde_json::json!({
         "AccountTag": account_id,
-        "TunnelSecret": tunnel_secret,
         "TunnelID": tunnel_id,
+        "TunnelSecret": tunnel_secret,
     });
 
     // Determine the directory path
@@ -170,8 +215,12 @@ fn save_tunnel_credentials(tunnel_id: &str, tunnel_secret: &str) -> Result<(), C
     let file_path = dir_path.join(format!("{}.json", tunnel_id));
 
     // Create and write to the file
-    let mut file = File::create(file_path).map_err(|err| CliError::StatusErr(err.to_string()))?;
-    write!(file, "{}", data).map_err(|err| CliError::StatusErr(err.to_string()))?;
+    let mut file: Box<dyn FileLike> = fs
+        .create_file(file_path)
+        .map_err(|err| CliError::StatusErr(err.to_string()))?;
+    let data_string = data.to_string();
+    fs.write_file(&mut file, &data_string)
+        .map_err(|err| CliError::StatusErr(err.to_string()))?;
 
     Ok(())
 }
@@ -201,62 +250,19 @@ fn create_config_yml(fs: &dyn FileSystem, tunnel_id: &str) -> Result<(), CliErro
     let mut file: Box<dyn FileLike> = fs
         .create_file(dir_path.join("config.yml"))
         .map_err(|err| CliError::StatusErr(err.to_string()))?;
-    fs.write_file(&mut file, serialized.as_bytes())
+    fs.write_file(&mut file, &serialized)
         .map_err(|err| CliError::StatusErr(err.to_string()))?;
     Ok(())
 }
 
-pub fn create_dns_record(tunnel_id: &str, tunnel_name: &str) -> Result<(), CliError> {
-    //let zone_id = env::var("LINKUP_CLOUDFLARE_ZONE_ID").map_err(|err| CliError::BadConfig(err.to_string()) )?;
-    let zone_id = "ZONE_ID";
-    let url = format!(
-        "https://api.cloudflare.com/client/v4/zones/{}/dns_records",
-        zone_id
-    );
-    let (client, headers) = prepare_client_and_headers()?;
-    let body = serde_json::to_string(&CreateDNSRecordRequest {
-        name: format!("tunnel-{}", tunnel_name),
-        content: format!("{}.cfargotunnel.com", tunnel_id),
-        r#type: "CNAME".to_string(),
-        proxied: true,
-    })
-    .map_err(|err| CliError::StatusErr(err.to_string()))?;
-
-    println!("{}", body);
-
-    send_request(&client, &url, headers, Some(body), "POST")
-}
-
-#[automock]
-trait FileSystem {
-    fn create_file(&self, path: PathBuf) -> Result<Box<dyn FileLike>, CliError>;
-    fn write_file(&self, file: &mut Box<dyn FileLike>, content: &[u8]) -> Result<(), CliError>;
-}
-
-struct RealFileSystem;
-
-impl FileSystem for RealFileSystem {
-    fn create_file(&self, path: PathBuf) -> Result<Box<dyn FileLike>, CliError> {
-        let file = File::create(path).map_err(|err| CliError::StatusErr(err.to_string()))?;
-        Ok(Box::new(file))
-    }
-
-    fn write_file(&self, file: &mut Box<dyn FileLike>, content: &[u8]) -> Result<(), CliError> {
-        file.write_all(content)
-            .map_err(|err| CliError::StatusErr(err.to_string()))?;
-        Ok(())
-    }
-}
-
-pub trait FileLike: Read + Write {}
-impl FileLike for std::fs::File {}
-
 #[cfg(test)]
 mod tests {
-
     use super::*;
+
+    use crate::file_system::MockFileSystem;
+
     use mockall::predicate;
-    use std::io::Result as IoResult;
+    use std::io::{Read, Write, Result as IoResult};
     struct MockFile {
         content: Vec<u8>,
     }
@@ -313,12 +319,45 @@ mod tests {
             .returning(|_| Ok(Box::new(MockFile::new()) as Box<dyn FileLike>));
         file_system_mock
             .expect_write_file()
-            .with(predicate::always(), predicate::eq(content.as_bytes()))
+            .with(predicate::always(), predicate::eq(content))
             .returning(|_, _| Ok(()));
 
         let result = create_config_yml(&file_system_mock, "TUNNEL_ID");
         assert!(result.is_ok());
 
         env::remove_var("HOME")
+    }
+
+    #[test]
+    fn test_save_tunnel_credentials() {
+        env::set_var("HOME", "/tmp/home");
+        env::set_var("LINKUP_CLOUDFLARE_ACCOUNT_ID", "ACCOUNT_ID");
+        let content = "{\"AccountTag\":\"ACCOUNT_ID\",\"TunnelID\":\"TUNNEL_ID\",\"TunnelSecret\":\"AQIDBAUGBwgBAgMEBQYHCAECAwQFBgcIAQIDBAUGBwg=\"}";
+
+        let mut file_system_mock = MockFileSystem::new();
+        file_system_mock
+            .expect_create_file()
+            .withf(|path| path.ends_with("TUNNEL_ID.json"))
+            .returning(|_| Ok(Box::new(MockFile::new()) as Box<dyn FileLike>));
+        file_system_mock
+            .expect_write_file()
+            .with(predicate::always(), predicate::eq(content))
+            .returning(|_, _| Ok(()));
+
+        let result = save_tunnel_credentials(
+            &file_system_mock,
+            "TUNNEL_ID",
+            "AQIDBAUGBwgBAgMEBQYHCAECAwQFBgcIAQIDBAUGBwg=",
+        );
+        assert!(result.is_ok());
+
+        env::remove_var("HOME");
+        env::remove_var("LINKUP_CLOUDFLARE_ACCOUNT_ID");
+    }
+
+    #[test]
+    fn test_generate_tunnel_secret() {
+        let secret = generate_tunnel_secret();
+        assert_eq!(secret.len(), 44);
     }
 }
