1ebbf584f80807ce1b48242a45038ce70cf74ea8
Jonas Lindström <jonas.lindstrom@mentimeter.com>
2024-05-30 09:15:33 +0200

    refactor,test: file_system -> system

--- linkup-cli/src/system.rs
+++ linkup-cli/src/system.rs
@@ -8,18 +8,21 @@ pub trait FileLike: Read + Write {}
 impl FileLike for std::fs::File {}
 
 #[cfg_attr(test, mockall::automock)]
-pub trait FileSystem {
+pub trait System {
+    fn get_env(&self, key: &str) -> Result<String, CliError>;
     fn create_file(&self, path: PathBuf) -> Result<Box<dyn FileLike>, CliError>;
     fn write_file(&self, file: &mut Box<dyn FileLike>, content: &str) -> Result<(), CliError>;
     fn file_exists(&self, file_path: &Path) -> bool;
     fn create_dir_all(&self, path: &Path) -> Result<(), CliError>;
-    fn get_home(&self) -> Result<String, CliError>;
-    fn get_env(&self, key: &str) -> Result<String, CliError>;
 }
 
-pub struct RealFileSystem;
+pub struct RealSystem;
 
-impl FileSystem for RealFileSystem {
+impl System for RealSystem {
+    fn get_env(&self, key: &str) -> Result<String, CliError> {
+        Ok(env::var(key).unwrap_or_else(|_| panic!("{} is not set", key)))
+    }
+
     fn create_file(&self, path: PathBuf) -> Result<Box<dyn FileLike>, CliError> {
         let file = File::create(path).map_err(|err| CliError::StatusErr(err.to_string()))?;
         Ok(Box::new(file))
@@ -37,13 +40,5 @@ impl FileSystem for RealFileSystem {
 
     fn create_dir_all(&self, path: &Path) -> Result<(), CliError> {
         fs::create_dir_all(path).map_err(|err| CliError::StatusErr(err.to_string()))
-    }
-
-    fn get_home(&self) -> Result<String, CliError> {
-        Ok(env::var("HOME").expect("HOME is not set"))
-    }
-
-    fn get_env(&self, key: &str) -> Result<String, CliError> {
-        Ok(env::var(key).unwrap_or_else(|_| panic!("{} is not set", key)))
     }
 }
