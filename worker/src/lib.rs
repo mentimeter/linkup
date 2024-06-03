57cc75ef5b22a24c23b5fbb2fb77438b85d0962e
Oliver Stenbom <oliver@stenbom.eu>
2024-06-03 09:06:58 +0200

    Allow request caching

--- worker/src/lib.rs
+++ worker/src/lib.rs
@@ -106,7 +106,26 @@ async fn linkup_request_handler(State(kv): State<KvStore>, mut req: Request) -> 
         }
     };
 
-    let worker_resp = match Fetch::Request(worker_req).send().await {
+    let cacheable_req = is_cacheable_request(&worker_req, &config);
+    let cache_key = get_cache_key(&worker_req, &session_name).unwrap();
+    if cacheable_req {
+        if let Some(worker_resp) = get_cached_req(cache_key.clone()).await {
+            let resp: HttpResponse = match worker_resp.try_into() {
+                Ok(resp) => resp,
+                Err(e) => {
+                    return HttpError::new(
+                        format!("Failed to parse response: {}", e),
+                        StatusCode::BAD_GATEWAY,
+                    )
+                    .into_response()
+                }
+            };
+
+            return resp.into_response();
+        }
+    }
+
+    let mut worker_resp = match Fetch::Request(worker_req).send().await {
         Ok(resp) => resp,
         Err(e) => {
             return HttpError::new(
@@ -120,6 +139,26 @@ async fn linkup_request_handler(State(kv): State<KvStore>, mut req: Request) -> 
     if is_websocket {
         handle_ws_resp(worker_resp).await.into_response()
     } else {
+        if cacheable_req {
+            let cache_clone = match worker_resp.cloned() {
+                Ok(resp) => resp,
+                Err(e) => {
+                    return HttpError::new(
+                        format!("Failed to clone response: {}", e),
+                        StatusCode::BAD_GATEWAY,
+                    )
+                    .into_response()
+                }
+            };
+
+            if let Err(e) = set_cached_req(cache_key, cache_clone).await {
+                return HttpError::new(
+                    format!("Failed to cache response: {}", e),
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                )
+                .into_response();
+            }
+        }
         handle_http_resp(worker_resp).await.into_response()
     }
 }
@@ -228,64 +267,52 @@ async fn no_tunnel() -> impl IntoResponse {
         .into_response()
 }
 
-// fn is_cacheable_request(req: &Request, config: &Session) -> bool {
-//     if req.method() != Method::Get {
-//         return false;
-//     }
+fn is_cacheable_request(req: &worker::Request, config: &Session) -> bool {
+    if req.method() != worker::Method::Get {
+        return false;
+    }
 
-//     if let Some(routes) = &config.cache_routes {
-//         let path = req.path();
-//         if routes.iter().any(|route| route.is_match(&path)) {
-//             return true;
-//         }
-//     }
+    if let Some(routes) = &config.cache_routes {
+        let path = req.path();
+        if routes.iter().any(|route| route.is_match(&path)) {
+            return true;
+        }
+    }
 
-//     false
-// }
+    false
+}
 
-// fn get_cache_key(req: &Request, session_name: &String) -> Option<String> {
-//     let mut cache_url = match req.url() {
-//         Ok(url) => url,
-//         Err(_) => return None,
-//     };
+fn get_cache_key(req: &worker::Request, session_name: &String) -> Option<String> {
+    let mut cache_url = match req.url() {
+        Ok(url) => url,
+        Err(_) => return None,
+    };
 
-//     let curr_domain = cache_url.domain().unwrap_or("example.com");
-//     if cache_url
-//         .set_host(Some(&format!("{}.{}", session_name, curr_domain)))
-//         .is_err()
-//     {
-//         return None;
-//     }
+    let curr_domain = cache_url.domain().unwrap_or("example.com");
+    if cache_url
+        .set_host(Some(&format!("{}.{}", session_name, curr_domain)))
+        .is_err()
+    {
+        return None;
+    }
 
-//     Some(cache_url.to_string())
-// }
+    Some(cache_url.to_string())
+}
 
-// async fn get_cached_req(req: &Request, session_name: &String) -> Option<Response> {
-//     let cache_key = match get_cache_key(req, session_name) {
-//         Some(cache_key) => cache_key,
-//         None => return None,
-//     };
+async fn get_cached_req(cache_key: String) -> Option<worker::Response> {
+    match worker::Cache::default().get(cache_key, false).await {
+        Ok(Some(resp)) => Some(resp),
+        _ => None,
+    }
+}
 
-//     match Cache::default().get(cache_key, false).await {
-//         Ok(Some(resp)) => Some(resp),
-//         _ => None,
-//     }
-// }
+async fn set_cached_req(cache_key: String, resp: worker::Response) -> worker::Result<()> {
+    // Cache API throws error on 206 partial content
+    if resp.status_code() > 499 || resp.status_code() == 206 {
+        return Ok(());
+    }
 
-// async fn set_cached_req(
-//     req: &Request,
-//     mut resp: Response,
-//     session_name: String,
-// ) -> Result<Response> {
-//     // Cache API throws error on 206 partial content
-//     if resp.status_code() > 499 || resp.status_code() == 206 {
-//         return Ok(resp);
-//     }
+    worker::Cache::default().put(cache_key, resp).await?;
 
-//     if let Some(cache_key) = get_cache_key(req, &session_name) {
-//         let cache_resp = resp.cloned()?;
-//         Cache::default().put(cache_key, cache_resp).await?;
-//     }
-
-//     Ok(resp)
-// }
+    Ok(())
+}
