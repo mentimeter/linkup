7871353aedd7c9eb16a68165127c79a0113670e3
Augusto César <augusto.silva@mentimeter.com>
2025-02-14 14:05:48 +0100

    refactor: restructure certificate cache endpoints

--- worker/src/routes/certificate_cache.rs
+++ worker/src/routes/certificate_cache.rs
@@ -1,37 +1,34 @@
-use axum::{extract::State, response::IntoResponse, routing::get, Json, Router};
+use axum::{
+    extract::{self, State},
+    response::IntoResponse,
+    routing::put,
+    Json, Router,
+};
 use http::StatusCode;
 
 use crate::LinkupState;
 
 pub fn router() -> Router<LinkupState> {
     Router::new().route(
-        "/linkup/certificate-cache",
-        get(get_certificate_cache_handler)
-            .post(create_certificate_cache_handler)
-            .put(update_certificate_cache_handler)
+        "/linkup/certificate-cache/{key}",
+        put(upsert_certificate_cache_handler)
+            .get(get_certificate_cache_handler)
             .delete(delete_certificate_cache_handler),
     )
 }
 
 #[worker::send]
-async fn get_certificate_cache_handler(State(_state): State<LinkupState>) -> impl IntoResponse {
+async fn get_certificate_cache_handler(
+    State(_state): State<LinkupState>,
+    extract::Path(_key): extract::Path<String>,
+) -> impl IntoResponse {
     (StatusCode::OK, "get_certificate_cache_handler stub").into_response()
 }
 
 #[worker::send]
-async fn create_certificate_cache_handler(
+async fn upsert_certificate_cache_handler(
     State(_state): State<LinkupState>,
-    Json(payload): Json<serde_json::Value>,
-) -> impl IntoResponse {
-    (
-        StatusCode::OK,
-        format!("create_certificate_cache_handler stub: {:?}", payload),
-    )
-}
-
-#[worker::send]
-async fn update_certificate_cache_handler(
-    State(_state): State<LinkupState>,
+    extract::Path(_key): extract::Path<String>,
     Json(payload): Json<serde_json::Value>,
 ) -> impl IntoResponse {
     (
@@ -41,6 +38,9 @@ async fn update_certificate_cache_handler(
 }
 
 #[worker::send]
-async fn delete_certificate_cache_handler(State(_state): State<LinkupState>) -> impl IntoResponse {
+async fn delete_certificate_cache_handler(
+    State(_state): State<LinkupState>,
+    extract::Path(_key): extract::Path<String>,
+) -> impl IntoResponse {
     (StatusCode::OK, "delete_certificate_cache_handler stub").into_response()
 }
