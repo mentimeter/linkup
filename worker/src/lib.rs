a9650394031656fbf0c415eabce2615f4a6d0d85
Augusto César <augusto.silva@mentimeter.com>
2026-02-27 15:10:39 +0100

    chore: bump getrandom on worker

--- worker/src/lib.rs
+++ worker/src/lib.rs
@@ -519,7 +519,7 @@ fn cloudflare_client(api_token: &str) -> cloudflare::framework::async_api::Clien
 
 pub fn generate_secret() -> String {
     let mut random_bytes = [0u8; 32];
-    getrandom::getrandom(&mut random_bytes).unwrap();
+    getrandom::fill(&mut random_bytes).unwrap();
 
     base64::Engine::encode(&base64::prelude::BASE64_STANDARD, random_bytes)
 }
