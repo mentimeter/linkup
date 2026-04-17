663ab7de961fc0fda0231de782e94ecc3f8d5943
Augusto César <augusto.silva@mentimeter.com>
2026-04-17 15:54:25 +0200

    refactor: unify tunnel models

--- linkup-clients/src/lib.rs
+++ linkup-clients/src/lib.rs
@@ -2,4 +2,4 @@ mod local_server;
 mod worker;
 
 pub use local_server::{Error as LocalServerClientError, LocalServerClient};
-pub use worker::{Error as WorkerClientError, TunnelData, WorkerClient};
+pub use worker::{Error as WorkerClientError, WorkerClient};
